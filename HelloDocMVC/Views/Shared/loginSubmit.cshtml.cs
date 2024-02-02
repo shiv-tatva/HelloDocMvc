8c1071055c1512da02d60e320a1acdf9dae16efd
shivsantoki7 <santoki.shiv.it@vvpedulink.ac.in>
2024-02-02 18:45:50 +0530

    all

---
+++ HelloDocMVC/Views/Shared/loginSubmit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HelloDocMVC.Views.Shared
+{
+    public class loginSubmitModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
